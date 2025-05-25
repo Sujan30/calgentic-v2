febc1951eb60d3b4315c9939cba2f6307841ad4d
Sujan30 <Nandikolsujan@gmail.com>
2025-05-25 02:52:45 -0700

    added api base url to api.ts file

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Define the base URL for your API
-export const API_BASE_URL = 'http://127.0.0.1:5001';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // Create axios instance with default config
 const api = axios.create({
